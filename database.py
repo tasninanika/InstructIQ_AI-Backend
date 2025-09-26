c56795b9f33d40f42b61d733a22408257d88d437
anikatasnin <jarintasnin09@gmail.com>
2025-09-26 20:50:05 +0600

    chore: setup MongoDB client and verify connection

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://<db_username>:<db_password>@cluster0.4vcsd99.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://<DB_USER>:<DB_PASS>@cluster0.4vcsd99.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
