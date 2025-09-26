df939cd1a061f0ebe9493a9e2c9e50388816f715
anikatasnin <jarintasnin09@gmail.com>
2025-09-26 20:53:06 +0600

    chore: setup MongoDB client and verify connection

--- database.py
+++ database.py
@@ -1,5 +1,12 @@
+import os
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
 
 uri = "mongodb+srv://<DB_USER>:<DB_PASS>@cluster0.4vcsd99.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
