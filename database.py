25aaab703b77b76c2c918ad4f65ecf656f5b1c87
anikatasnin <jarintasnin09@gmail.com>
2025-10-03 21:02:25 +0600

    chore: use environment variables for MongoDB credentials

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ load_dotenv()
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 
-uri = "mongodb+srv://{DB_USER}:{DB_PASS}@cluster0.4vcsd99.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = f"mongodb+srv://{DB_USER}:{DB_PASS}@cluster0.4vcsd99.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
