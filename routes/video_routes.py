67862bdf067e676c516ab309343f9e55e087975a
anikatasnin <jarintasnin09@gmail.com>
2025-10-03 22:31:42 +0600

    chore: setup router and ensure video/audio folders exist

--- routes/video_routes.py
+++ routes/video_routes.py
@@ -4,3 +4,9 @@ import os
 import moviepy as mp
 import whisper
 from database import videos_collection
+
+router = APIRouter()
+
+# folders 
+os.makedirs("uploads/videos", exist_ok=True)
+os.makedirs("uploads/audios", exist_ok=True)
